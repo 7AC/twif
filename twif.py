6212279bc2c8e2c4853220cbd8e41cdef75a43ac
Giuseppe Valente <gvalente@arista.com>
2015-01-07 10:37:42 -0800

    --verbose

--- twif.py
+++ twif.py
@@ -4,8 +4,9 @@ from pync import Notifier
 from TwitterSearch import TwitterSearch, TwitterSearchOrder
 import argparse
 from datetime import datetime
+import requests
 
-def search( ts, keywords, maxAge=600, debug=False ):
+def search( ts, keywords, maxAge=600, verbose=False, debug=False ):
    tso = TwitterSearchOrder()
    tso.set_keywords( keywords )
    tso.set_include_entities( False )
@@ -23,6 +24,8 @@ def search( ts, keywords, maxAge=600, debug=False ):
          Notifier.notify( text, title=title, subtitle='@%s' % screenName,
                           sender='com.twitter.twitter-mac',
                           activate='com.apple.Safari', open=url )
+         if verbose:
+            print url
          if debug:
             break
 
@@ -41,10 +44,16 @@ parser.add_argument( '-d', '--debug', action='store_true', help='debug mode' )
 parser.add_argument( '-k', '--keyword', action='append', help='keyword to search' )
 parser.add_argument( '-m', '--max-age', metavar='MINUTES', type=int, default=10,
                      help='maximum tweet age' )
+parser.add_argument( '-v', '--verbose', action='store_true', help='verbose mode' )
 args = parser.parse_args()
 
-ts = TwitterSearch( consumer_key=args.consumer_key,
-                    consumer_secret=args.consumer_secret,
-                    access_token=args.access_token,
-                    access_token_secret=args.access_token_secret )
-search( ts, args.keywords, maxAge=args.max_age * 60, debug=args.debug )
+try:
+   ts = TwitterSearch( consumer_key=args.consumer_key,
+                       consumer_secret=args.consumer_secret,
+                       access_token=args.access_token,
+                       access_token_secret=args.access_token_secret )
+   search( ts, args.keywords, maxAge=args.max_age * 60, verbose=args.verbose,
+           debug=args.debug )
+except requests.exceptions.ConnectionError, e:
+   if args.verbose:
+      print e
